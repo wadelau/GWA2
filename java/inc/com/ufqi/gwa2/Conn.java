778840a8a267988e528761e19d0d2fe2632d6c9c
Wadelau <wadelau@gmail.com>
2016-06-28 18:34:11 +0800

    inc updates.

---
+++ java/inc/com/ufqi/gwa2/Conn.java
@@ -0,0 +1,42 @@
+/* DB and others Connection confisg, for all settings.
+ * v0.1
+ * wadelau@ufqi.com
+ * since Wed Jul 13 18:20:28 UTC 2011
+ * Ported into Java by wadelau@ufqi.com, June 28, 2016
+ */
+
+
+package com.ufqi.gwa2;
+
+
+import java.util.HashMap;
+
+
+import com.ufqi.gwa2.Config;
+
+
+public final static class Conn {
+
+	
+	static class DbConnMaster{
+		
+		//- @todo
+		
+	}
+	
+	
+	static class DbConnSlave{
+		
+		//- @todo
+		
+	}
+	
+	
+	static class CacheConnMaster{
+		
+		//- @todo
+		
+	}
+	
+	
+}
