0fd3dd559a96c099d8d5a6ff1ae69ff983aeb65c
Wadelau <wadelau@gmail.com>
2016-12-02 18:19:54 +0800

    new common modules: logx and tablex.

---
+++ php/mod/logx.class.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * app's log handler
+ * init by Xenxin@Pbtt
+ * Thu, 24 Nov 2016 19:53:45 +0800
+ */
+
+if(!defined('__ROOT__')){
+    define('__ROOT__', dirname(dirname(__FILE__)));
+}
+require_once(__ROOT__.'/inc/webapp.class.php');
+
+class LogX extends WebApp {
+    
+    //- variables
+    var $logf = '';
+    
+    //- construct
+    public function __construct($args){
+        # self works
+        if(isset($args['logfile'])){
+            $this->logf = $args['logfile'];
+        }
+        else{
+            $this->logf = GConf::get('logfile');
+        }
+        
+        parent::__construct($args);
+    }
+    
+    //- destruct
+    public function __destruct(){
+        # @todo
+    }
+    
+    //- methods
+    public function say($msg, $args=null){
+        $rtn = true;
+        $logf = $this->logf;
+        $mytime = time();
+        $logf .= date("Ymd", $mytime).'.log';
+        $hm = $this->setBy('file:', array('target'=>$logf, 'reuse'=>true,
+                'isappend'=>true,
+                'content'=>date("Y-m-d-H:i:sO", $mytime).' '.$msg."\n",
+        ));
+        return $rtn;
+    }
+    
+    //- inner facility
+
+}
+?>
